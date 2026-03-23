684112e0899a20ee919e9419fc10940285f7e0fb
ChocoChocobo <sharonovjr@yahoo.com>
2026-03-23 16:35:13 +0300

    rpo-invaders ч.1

--- SFMLRPG/SFMLRPG.cpp
+++ SFMLRPG/SFMLRPG.cpp
@@ -1,52 +1,81 @@
-﻿#include <iostream>
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
+#include <vector>
 
 using namespace sf;
 
-int main()
+// Структура пули, имеющая фигуру прямоугольника и флаг, говорящий о существовании объекта
+struct Bullet
 {
-    // RenderWindow - это класс, ответственный за создание графического окна. Наследуется от более низкоуровневого кода, который взаимодействует с OpenGL.
-    // VideoMode - класс, определяющий свойства окна. Напирмер: его размер.
-    RenderWindow mainWindow(VideoMode({ 800, 600 }), "Test");
+    CircleShape bulletShape;
+    bool isDestroyed = false;
+};
+// Структура врага, имеющая фигуру прямоугольника и флаг, говорящий о существовании объекта
+struct Enemy
+{
+    RectangleShape enemyShape;
+    bool isDestroyed = false;
+};
 
-    // Создание объекта круга
-    CircleShape basicCircle(90.f);
-    basicCircle.setFillColor(Color::White);
-    basicCircle.setOutlineThickness(5.f);
-    basicCircle.setOutlineColor(Color::Blue);
+int main()
+{    
+    // Инициализация окна
+    unsigned int width = 800;
+    unsigned int height = 600;
+    RenderWindow mainWindow(VideoMode({ width, height }), "RPO Invaders");
 
-    // Установка центра фигуры
-    basicCircle.setOrigin({ 20.f, 40.f });
-    float basicCircleSpeed = 5.f;
-    // Функция setPosition для установки положения объекта в окне
-    Vector2f circlePosition = { 310.f, 210.f };
-    basicCircle.setPosition(circlePosition);
+    // Инициализация игрока
+    RectangleShape playerShape;
+    playerShape.setSize({ 60.0f, 20.0f });
+    playerShape.setFillColor(Color::Yellow);
+    playerShape.setOutlineThickness(5.0f);
+    playerShape.setOutlineColor(Color::Blue);
+    playerShape.setOrigin({ 30.0f, 10.0f });
+    playerShape.setPosition({ width / 2.0f, height - 10.0f });    
 
-    while (mainWindow.isOpen())
-    {
-        // Необходимо обрабатывать события каждый кадр. Если быть точнее, то необходимо обрабатывать события, поступившие с момента прошлого кадра.
-        // Происходит создание объекта события, в который помещается обрабатываемое событие окна каждый кадр
+    // Инициализация других объектов
+    std::vector<Bullet> bulletsArray;
+    std::vector<Enemy> enemiesArray;
+    // ...
+
+    // Таймеры
+    Clock framesClock; // используется для deltaTime
+    Clock spawnEnemyClock;
+    Clock spawnBulletClock;
+
+    // Скорость объектов
+    const float playerSpeed = 200.0f;
+    const float enemySpeed = 175.0f;
+    const float bulletSpeed = 400.0f;
+
+    // Флаг конца игры
+    bool isGameOver = false;
+
+    while (mainWindow.isOpen() && !isGameOver)
+    {        
         while (const std::optional event = mainWindow.pollEvent())
         {
-            // Проверка закрытия окна
-            // Обращаясь к событию, которое возникло у окна, через указатель возможно вызвать функцию is, которая в качестве аргумента принимает шаблонное действие события
             if (event->is<Event::Closed>())
             {
                 mainWindow.close();
             }
+        } 
+        float playerDirection = 0.0f; // Направление игрока влево вправо
+        // delta time - отношение кадра к предыдущему. Умножать на данное значение необходимо затем, чтобы количество кадров при разном количестве кадров в секунду, было одинаковым.
+        float deltaTime = framesClock.restart().asSeconds();
+        // Если нажата клавиша A или Стрелка влево, то изменяем направление движения фигуры игрока
+        if (Keyboard::isKeyPressed(Keyboard::Key::A) || Keyboard::isKeyPressed(Keyboard::Key::Left))
+        {
+            playerDirection -= 1.0f;
         }
+        if (Keyboard::isKeyPressed(Keyboard::Key::D) || Keyboard::isKeyPressed(Keyboard::Key::Right))
+        {
+            playerDirection += 1.0f;
+        }
+        playerShape.move({ playerDirection * playerSpeed * deltaTime, 0.0f });
 
-        // Логика вращения круга
-        basicCircle.move({ basicCircleSpeed, 0.f });
-        basicCircle.move({ -basicCircleSpeed, 0.f });
-        basicCircle.rotate(degrees(0.5f));
-        basicCircle.setOrigin({ 20.0f, 40.0f });
+        mainWindow.clear(Color(0, 0, 0, 255)); 
 
-        // очистка окна, что бы круги не наслаивались друг на друга
-        mainWindow.clear(Color(0, 0, 0, 255));
-        // Окно является контейнером для того, что необходимо отрисовать на экране. Воспроизвести содержимое можно с помощью функции dispaly()
-        mainWindow.draw(basicCircle);
-        // Показываем окно
+        mainWindow.draw(playerShape);
         mainWindow.display();
     }
 
